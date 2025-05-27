94cd050c207f2ccf6ea22f23c83d544aa9874c3c
Jongmin Choi <jongmine.ch@gmail.com>
2025-05-27 20:12:43 +0900

    Update fetchPokemonList.mjs

--- pokency-serverless/functions/pokemon/service/fetchPokemonList.mjs
+++ pokency-serverless/functions/pokemon/service/fetchPokemonList.mjs
@@ -1,6 +1,9 @@
 export async function fetchPokemonList(limit = 20, offset = 0) {
+  console.time(`[fetchPokemonList] 전체`);
   const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  console.time(`[fetchPokemonList] pokemon 리스트`);
   const response = await fetch(url);
+  console.timeEnd(`[fetchPokemonList] pokemon 리스트`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch Pokémon list");
@@ -8,33 +11,33 @@ export async function fetchPokemonList(limit = 20, offset = 0) {
 
   const data = await response.json();
 
+  console.time(`[fetchPokemonList] species 한글명 fetch`);
   const localizedList = await Promise.all(
     data.results.map(async (pokemon) => {
       const res = await fetch(
         `https://pokeapi.co/api/v2/pokemon-species/${pokemon.name}`
       );
-      let koreanName = pokemon.name;
-      if (res.ok) {
-        const species = await res.json();
-        const koreanNameEntry = species.names.find(
-          (entry) => entry.language.name === "ko"
-        );
-        if (koreanNameEntry) {
-          koreanName = koreanNameEntry.name;
-        }
+      if (!res.ok) {
+        return {
+          name: pokemon.name,
+          url: pokemon.url,
+        };
       }
-      // id 추출
-      const id = Number(pokemon.url.split("/").filter(Boolean).pop());
+      const species = await res.json();
+      const koreanNameEntry = species.names.find(
+        (entry) => entry.language.name === "ko"
+      );
+      const koreanName = koreanNameEntry ? koreanNameEntry.name : pokemon.name;
+
       return {
         name: pokemon.name,
         name_ko: koreanName,
         url: pokemon.url,
-        id,
-        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-        official_artwork: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
       };
     })
   );
+  console.timeEnd(`[fetchPokemonList] species 한글명 fetch`);
+  console.timeEnd(`[fetchPokemonList] 전체`);
 
   return localizedList;
 }
